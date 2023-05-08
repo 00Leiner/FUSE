a5d30d9479228a423e3a5063e3e7b25f2c1d7bf0
00Leiner <leinersacdalan4@gmail.com>
2023-05-08 20:40:59 +0800

    all

--- Fuseprofiling/registry.vb
+++ Fuseprofiling/registry.vb
@@ -95,9 +95,13 @@ Public Class registry
             Return
         End If
         If Not contactbox.Text.StartsWith("09") Then
-            MessageBox.Show("Please fill the Contact No.")
+            MessageBox.Show("Incorrect Contact Number Format")
             Return
         End If
+        If contactbox.Text.Length <> 11 Then
+            MessageBox.Show("Incorrect Contact Number Format")
+        End If
+
         'declare variables
         Dim SURNAME As String = surnamebox.Text
         Dim FIRSTNAME As String = firstbox.Text
